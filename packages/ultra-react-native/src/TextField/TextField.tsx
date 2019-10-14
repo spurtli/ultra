1419a5aa3ad090533735c0097b44da429b81d1d8
Hannes Moser <hannes.moser@shopify.com>
2019-10-14 16:48:05 -0400

    feat: add support for label to textfields

--- packages/ultra-react-native/src/TextField/TextField.tsx
+++ packages/ultra-react-native/src/TextField/TextField.tsx
@@ -1,11 +1,46 @@
-import styled from '../styled-components';
-import {Color} from '../theme';
+import React, {PropsWithChildren} from 'react';
 
-export const TextField = styled.TextInput`
-  color: ${({theme}): Color => theme.colors.textColor};
+import styled from '../styled-components';
+
+const Label = styled.Text`
+  color: ${({theme}) => theme.colors.tintColor};
+`;
+
+const TextInput = styled.TextInput`
+  color: ${({theme}) => theme.colors.textColor};
   font-size: 20;
   height: 40;
-  border-bottom-color: ${({theme}): Color => theme.colors.tint1};
+  border-bottom-color: ${({theme}) => theme.colors.tint1};
   border-bottom-width: 1;
   margin-bottom: 16;
 `;
+
+type TextFieldProps = {
+  label: string;
+  value: string;
+  clearMode?: 'while-editing';
+  placeholder?: string;
+  onChange?(value: string): void;
+};
+type Props = PropsWithChildren<TextFieldProps>;
+
+export function TextField({
+  label,
+  value,
+  clearMode = 'while-editing',
+  placeholder = '',
+  onChange = () => {}
+}: Props) {
+  return (
+    <>
+      {label && <Label>{label}</Label>}
+      <TextInput
+        accessibilityLabel={label}
+        value={value}
+        placeholder={placeholder}
+        clearButtonMode={clearMode}
+        onChangeText={onChange}
+      />
+    </>
+  );
+}
